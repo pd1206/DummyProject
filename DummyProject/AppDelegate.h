8b3e0b1764407e9efabcf6a0044d04346ddcf370
labuser <labuser@lab-mac-2ec452.corp.adobe.com>
2016-09-23 16:25:45 +0530

    initial commit

---
+++ DummyProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DummyProject
+//
+//  Created by labuser on 9/23/16.
+//  Copyright © 2016 labuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
